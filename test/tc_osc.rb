cb22005b9ddd8cce129a282eba6905c37d304539
hans <hans@fugal.net>
2006-03-07 19:10:38 +0100

    test skeleton
    darcs-hash:20060307181038-28bda-87901ad4ce791aed915a21a63c8a93b8c82cfa06.gz

---
+++ test/tc_osc.rb
@@ -0,0 +1,24 @@
+require 'osc'
+require 'test/unit'
+
+class TC_OSC < Test::Unit::TestCase
+  include OSC
+  # def setup
+  # end
+
+  # def teardown
+  # end
+
+  def test_datatype
+  end
+  def test_message
+  end
+  def test_bundle
+  end
+  def test_packet
+  end
+  def test_server
+  end
+  def test_client
+  end
+end
